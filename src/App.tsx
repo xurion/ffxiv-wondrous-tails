def751e5eba016fbaf9d16200cbe1f4c0472e3de
Dean James <xurion@hotmail.com>
2021-01-16 15:55:52 +0000

    Grid/cells working. Create possible combo arrays. Create preview area of all possible combos. Add pic of all combos.

--- src/App.tsx
+++ src/App.tsx
@@ -1,95 +1,110 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import getAllCombinations, { Combination } from "./combinations";
+import WinningComboGrid from "./WinningComboGrid";
 
 type CellProps = {
-  selected?: boolean;
+  active?: boolean;
 };
 const Cell = styled.td<CellProps>`
   ${(props) => `
-    background: ${props.selected ? "#ccc" : "#fff"};
+    background: ${props.active ? "#ccc" : "#fff"};
+    border: 1px solid #999;
     cursor: pointer;
-    font-size: 100px;
-    padding: 0 30px;
-    text-align: center;
+    height: 100px;
+    width: 100px;
 
     &:hover {
-      
+      border-color: #333;
     }
   `}
 `;
 
 function App() {
-  const [cellStates, setCellStates] = useState<boolean[]>(
-    new Array(16).fill(false)
+  const [cellStates, setCellStates] = useState<Combination>(
+    new Array(16).fill(false) as Combination
   );
 
   const handleCellClick = (cell: number) => {
-    const newCellsState = [...cellStates];
+    if (getActiveCount() === 9 && cellStates[cell] === false) {
+      return;
+    }
+
+    const newCellsState: Combination = [...cellStates];
     newCellsState[cell] = !cellStates[cell];
     setCellStates(newCellsState);
   };
 
+  const getActiveCount = () =>
+    cellStates.reduce((prev, curr) => (curr ? prev + 1 : prev), 0);
+
+  const compareCombos = (w: Combination, c: Combination): boolean => {
+    const allFalse = c.indexOf(true) === -1;
+    if (allFalse) {
+      return true;
+    } else {
+      for (let i = 0; i < 16; i++) {
+        if (c[i] === true && w[i] === true) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  };
+
+  const isPotentialThreeLineCombo = (combination: Combination): boolean => {
+    const winningCombos = getAllCombinations();
+    let potentialThreeLineCombo = false;
+    winningCombos.forEach((winningCombo) => {
+      if (compareCombos(winningCombo, combination)) {
+        potentialThreeLineCombo = true;
+        return;
+      }
+    });
+
+    return potentialThreeLineCombo;
+  };
+
   return (
-    <table>
-      <tbody>
-        <tr>
-          <Cell selected={cellStates[0]} onClick={() => handleCellClick(0)}>
-            1
-          </Cell>
-          <Cell selected={cellStates[1]} onClick={() => handleCellClick(1)}>
-            2
-          </Cell>
-          <Cell selected={cellStates[2]} onClick={() => handleCellClick(2)}>
-            3
-          </Cell>
-          <Cell selected={cellStates[3]} onClick={() => handleCellClick(3)}>
-            4
-          </Cell>
-        </tr>
-        <tr>
-          <Cell selected={cellStates[4]} onClick={() => handleCellClick(4)}>
-            5
-          </Cell>
-          <Cell selected={cellStates[5]} onClick={() => handleCellClick(5)}>
-            6
-          </Cell>
-          <Cell selected={cellStates[6]} onClick={() => handleCellClick(6)}>
-            7
-          </Cell>
-          <Cell selected={cellStates[7]} onClick={() => handleCellClick(7)}>
-            8
-          </Cell>
-        </tr>
-        <tr>
-          <Cell selected={cellStates[8]} onClick={() => handleCellClick(8)}>
-            9
-          </Cell>
-          <Cell selected={cellStates[9]} onClick={() => handleCellClick(9)}>
-            10
-          </Cell>
-          <Cell selected={cellStates[10]} onClick={() => handleCellClick(10)}>
-            11
-          </Cell>
-          <Cell selected={cellStates[11]} onClick={() => handleCellClick(11)}>
-            12
-          </Cell>
-        </tr>
-        <tr>
-          <Cell selected={cellStates[12]} onClick={() => handleCellClick(12)}>
-            13
-          </Cell>
-          <Cell selected={cellStates[13]} onClick={() => handleCellClick(13)}>
-            14
-          </Cell>
-          <Cell selected={cellStates[14]} onClick={() => handleCellClick(14)}>
-            15
-          </Cell>
-          <Cell selected={cellStates[15]} onClick={() => handleCellClick(15)}>
-            16
-          </Cell>
-        </tr>
-      </tbody>
-    </table>
+    <>
+      <div>{getActiveCount()}/9</div>
+      <table>
+        <tbody>
+          <tr>
+            <Cell active={cellStates[0]} onClick={() => handleCellClick(0)} />
+            <Cell active={cellStates[1]} onClick={() => handleCellClick(1)} />
+            <Cell active={cellStates[2]} onClick={() => handleCellClick(2)} />
+            <Cell active={cellStates[3]} onClick={() => handleCellClick(3)} />
+          </tr>
+          <tr>
+            <Cell active={cellStates[4]} onClick={() => handleCellClick(4)} />
+            <Cell active={cellStates[5]} onClick={() => handleCellClick(5)} />
+            <Cell active={cellStates[6]} onClick={() => handleCellClick(6)} />
+            <Cell active={cellStates[7]} onClick={() => handleCellClick(7)} />
+          </tr>
+          <tr>
+            <Cell active={cellStates[8]} onClick={() => handleCellClick(8)} />
+            <Cell active={cellStates[9]} onClick={() => handleCellClick(9)} />
+            <Cell active={cellStates[10]} onClick={() => handleCellClick(10)} />
+            <Cell active={cellStates[11]} onClick={() => handleCellClick(11)} />
+          </tr>
+          <tr>
+            <Cell active={cellStates[12]} onClick={() => handleCellClick(12)} />
+            <Cell active={cellStates[13]} onClick={() => handleCellClick(13)} />
+            <Cell active={cellStates[14]} onClick={() => handleCellClick(14)} />
+            <Cell active={cellStates[15]} onClick={() => handleCellClick(15)} />
+          </tr>
+        </tbody>
+      </table>
+      <div>
+        Potential combination:{" "}
+        {isPotentialThreeLineCombo(cellStates).toString()}
+      </div>
+      {getAllCombinations().map((combo) => (
+        <WinningComboGrid combo={combo} />
+      ))}
+    </>
   );
 }
 
---
+++ src/combinations.ts
@@ -0,0 +1,80 @@
+// prettier-ignore
+export type Combination = [boolean, boolean, boolean, boolean, boolean, boolean, boolean, boolean, boolean, boolean, boolean, boolean, boolean, boolean, boolean, boolean];
+
+// prettier-ignore
+const getAllCombinations = (): Combination[] => [
+  [ //A
+    true,true,true,true,false,true,false,true,false,false,true,true,false,false,false,true
+  ],
+  [ //B
+    true,true,true,true,false,true,false,false,false,true,true,false,false,true,false,true
+  ],
+  [ //C
+    false,true,false,true,false,true,true,false,false,true,false,false,true,true,true,true
+  ],
+  [ //D
+    true,true,true,true,false,true,true,false,false,false,true,false,false,false,true,true
+  ],
+  [ //E
+    true,false,false,false,true,true,false,false,true,true,true,true,true,false,false,true
+  ],
+  [ //F
+    true,false,true,false,true,true,true,true,false,false,true,false,false,false,true,true
+  ],
+  [ //G
+    true,false,false,false,true,true,false,false,true,false,true,false,true,true,true,true
+  ],
+  [ //H
+    true,false,false,true,false,true,false,true,true,true,true,true,false,false,false,true
+  ],
+  [ //I
+    false,false,true,true,false,false,true,false,false,true,true,false,true,true,true,true
+  ],
+  [ //J
+    false,false,true,true,false,false,true,false,false,true,true,false,true,true,true,true
+  ],
+  [ //K
+    false,false,false,true,false,false,true,true,true,true,true,true,true,false,false,true
+  ],
+  [ //L
+    true,true,false,false,false,true,false,false,true,true,true,true,false,true,false,true
+  ],
+  [ //M
+    true,true,true,true,true,false,true,false,true,true,false,false,true,false,false,false
+  ],
+  [ //N
+    true,false,true,false,false,true,true,false,false,false,true,false,true,true,true,true
+  ],
+  [ //O
+    false,false,false,true,true,true,true,true,false,true,false,true,true,false,false,true
+  ],
+  [ //P
+    true,false,false,true,true,true,true,true,true,true,false,false,true,false,false,false
+  ],
+  [ //Q
+    true,false,false,true,true,true,true,true,false,false,true,true,false,false,false,true
+  ],
+  [ //R
+    false,true,false,true,true,true,true,true,false,true,false,false,true,true,false,false
+  ],
+  [ //S
+    false,false,false,true,false,false,true,true,false,true,false,true,true,true,true,true
+  ],
+  [ //T
+    true,false,false,false,true,true,true,true,true,false,true,false,true,false,false,true
+  ],
+  [ //U
+    true,true,true,true,false,false,true,false,false,true,true,false,true,false,true,false
+  ],
+  [ //v
+    true,true,true,true,false,true,true,false,false,true,false,false,true,true,false,false
+  ],
+  [ //W
+    true,true,false,false,false,true,false,false,false,true,true,false,true,true,true,true
+  ],
+  [ //x
+    false,false,true,true,false,false,true,false,true,true,true,true,true,false,true,false
+  ]
+];
+
+export default getAllCombinations;
