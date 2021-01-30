433f37354950b496fc7f2c35d4d3431e6676d3e7
Dean James <xurion@hotmail.com>
2021-01-30 14:27:06 +0000

    Add loading spinner to reshuffle.

---
+++ src/LoadingIcon.tsx
@@ -0,0 +1,16 @@
+import CircleLoader from "react-spinners/CircleLoader";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  left: 690px;
+  position: absolute;
+  top: 515px;
+`;
+
+const LoadingIcon = () => (
+  <Wrapper>
+    <CircleLoader color="#66b19f" size="100" />
+  </Wrapper>
+);
+
+export default LoadingIcon;
